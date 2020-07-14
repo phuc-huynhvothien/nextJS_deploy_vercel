f1348aaf49f6ecb26546a9f87ef5e8d219ab36c8
phuchvt <huynhvothienphuc963@gmail.com>
2020-07-14 21:01:11 +0700

    Styled Banner Component

---
+++ src/components/Banner/Banner.styled.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+export const StyleBannerLayout = styled.div`
+padding-bottom: 70px;
+padding-top: 70px;
+background-image: url(${props =>props.url ? props.url : ""});
+`
+export const StyleBannerContainer = styled.div`
+  padding-left : 15px;
+  padding-right: 15px;
+  width : 100%;
+`
+
+export const StyleBannerTitle = styled.h1`
+font-size: 30px;
+line-height: 1.3;
+margin-bottom: 20px;
+color: #333;
+`
+export const StyleBannerUrl = styled.p`
+margin-right: 30px;
+font-size: 16px;
+color: #777;
+`
--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
-const Banner = () => {
-
+import { IBanner } from '../../models/IBanner';
+import { Button } from 'react-bootstrap';
+import {
+    StyleBannerLayout,
+    StyleBannerContainer,
+    StyleBannerTitle,
+    StyleBannerUrl
+} from '../Banner/Banner.styled'
+const Banner: React.FC<IBanner> = (props) => {
+    const url = "";
+    return <>
+        <StyleBannerLayout url={url}>
+            <StyleBannerContainer>
+                <div className="row">
+                    <div className="col">
+                        <StyleBannerTitle>Shop Left Sidebar</StyleBannerTitle>
+                        <StyleBannerUrl>Home/Shop Left Sidebar </StyleBannerUrl>
+                    </div>
+                </div>
+            </StyleBannerContainer>
+        </StyleBannerLayout>
+    </>
 }
 export default Banner; 
