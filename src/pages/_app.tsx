d5f4a01fde8f3778d94271c85af8cfd255d932c3
huynhvothienphuc963 <huynhvothienphuc9631@gmail.com>
2020-07-24 22:48:13 +0700

    rm bs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
